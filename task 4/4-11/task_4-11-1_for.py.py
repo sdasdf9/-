5c684185d7582f9898970f7fa5b8b34f61c39773
sdasdf9 <markmalov2006@gmail.com>
2026-06-06 14:45:03 +0300

    Delete task 4 directory

--- task 4/4-11/task_4-11-1_for.py.py
+++
@@ -1,9 +0,0 @@
-vector1 = [1, 2, 3]
-vector2 = [4, 5, 6]
-
-scalar_product = 0
-
-for i in range(len(vector1)):
-    scalar_product += vector1[i] * vector2[i]
-
-print("Скалярное произведение векторов:", scalar_product)
--- task 4/4-13/task_4-13-2.py
+++
@@ -1,8 +0,0 @@
-a = float(input("Введите число A: "))
-b = float(input("Введите число B: "))
-c = float(input("Введите число C: "))
-d = float(input("Введите число D: "))
-
-avg = (a + b + c + d) / 4
-
-print("Среднее арифметическое:", avg)
